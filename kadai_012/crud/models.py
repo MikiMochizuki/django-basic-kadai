c52022315a0569af8849864b616ff837f1199c84
モチヅキミキ <tonton0312tonton@gmail.com>
2024-08-30 21:33:46 +0900

    kadai_012を修正しました。

--- kadai_012/crud/models.py
+++ kadai_012/crud/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.urls import reverse
 
 class Category(models.Model):
-     name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
  
-     def __str__(self):
-         return self.name
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -13,10 +13,12 @@ class Product(models.Model):
     price = models.PositiveIntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(blank=True, default='noImage.png')
+    detail = models.TextField(blank=True, null=True)  # 修正箇所: _()を削除し、シンタックスエラーを修正
 
     def __str__(self):
-         return self.name
+        return self.name
 
     # 新規作成・編集完了時のリダイレクト先
     def get_absolute_url(self):
-         return reverse('list')
+        return reverse('list')
+
